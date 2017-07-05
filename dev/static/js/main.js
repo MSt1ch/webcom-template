0fdf5cf1172a31cc9c7c6a35e8648cd8b7b06212
St1ch <st1ch@mail.ru>
2017-07-05 20:00:45 +0300

    errors

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,4 +1,5 @@
 ;
+<<<<<<< HEAD
 
 	
 
@@ -39,6 +40,25 @@
 					items : 1,
 					
 				}
+=======
+// Начинать писать отсюда!!!!
+$(function() {
+
+	$(".owl-carousel").owlCarousel({
+		Default: 1,
+		loop:true,
+		animateOut: 'fadeOut',
+    smartSpeed: 1500,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		resposiveClass: true,
+    nav: true,
+    dots: true,
+    autoHeight:true,
+		responsive : {
+			0 : {
+				items : 1,
+				
+>>>>>>> parent of 4574963... add slide separator
 			}
 			
 		});
