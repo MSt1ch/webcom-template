b00c507af8ce4d0f66ca5350471e8c68356c5595
St1ch <st1ch@mail.ru>
2017-07-05 20:04:37 +0300

    fixed finish revert

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,4 @@
 ;
-<<<<<<< HEAD
 
 	
 
@@ -40,25 +39,6 @@
 					items : 1,
 					
 				}
-=======
-// Начинать писать отсюда!!!!
-$(function() {
-
-	$(".owl-carousel").owlCarousel({
-		Default: 1,
-		loop:true,
-		animateOut: 'fadeOut',
-    smartSpeed: 1500,
-    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		resposiveClass: true,
-    nav: true,
-    dots: true,
-    autoHeight:true,
-		responsive : {
-			0 : {
-				items : 1,
-				
->>>>>>> parent of 4574963... add slide separator
 			}
 			
 		});
